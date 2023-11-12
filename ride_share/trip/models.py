c721d057eebda9bd0ded65f2a0f61fe5b61a82d5
renugopal <renugopal17@hotmail.com>
2023-11-11 22:48:28 -0600

    Add Car model

--- ride_share/trip/models.py
+++ ride_share/trip/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Trip(models.Model):
-    driver = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    # one user can have many Trips
+    driver = models.ForeignKey(User, on_delete=models.CASCADE)
+    # we do not delete users, we only soft delete, if we delete then we delete all user data so we cascade
     start_time = models.DateTimeField("Ride start Time")
     end_time = models.DateTimeField("Estimated reach time")
     source = models.CharField(max_length=200)
     destination = models.CharField(max_length=200)
+    seats = models.IntegerField(default=0)
+
+
+class Car(models.Model):
+    driver = models.ForeignKey(User,  on_delete=models.CASCADE)
+    car_number = models.CharField(max_length=200)
+    car_details = models.CharField(max_length=200)
 
 
