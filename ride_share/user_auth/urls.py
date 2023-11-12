c03b08c71213e55f55cfe5c9382c5bd01bd79c51
Kalyan Cheerla <32354220+kalyancheerla@users.noreply.github.com>
2023-11-12 10:55:09 -0600

    added login, logout, and sample profile, dashboard

--- ride_share/user_auth/urls.py
+++ ride_share/user_auth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
-from .views import SignUpView
-
+from .views import SignUpView, EagleLoginView, EagleLogoutView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path("login/", SignUpView.as_view(), name="user_login"),
-    path("signout/", SignUpView.as_view(), name="signout"),
+    path("login/", EagleLoginView.as_view(), name="login"),
+    path("dashboard/", TemplateView.as_view(template_name="dashboard.html"), name="dashboard"),
+    path("profile/", TemplateView.as_view(template_name="view_profile.html"), name="view_profile"),
+    path('logout/', EagleLogoutView.as_view(), name='logout'),
 ]
--- ride_share/user_auth/views.py
+++ ride_share/user_auth/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+class EagleLoginView(LoginView):
+    form_class = AuthenticationForm
+    success_url = reverse_lazy("dashboard")
+    template_name = "login.html"
+
+    def get_success_url(self):
+        return self.success_url
+
+class EagleLogoutView(LogoutView):
+    success_url = reverse_lazy("home")
+
+    def get_success_url(self):
+        return self.success_url
