98150119815e35eed0317c0769aa947edc95f0e3
Kalyan Cheerla <32354220+kalyancheerla@users.noreply.github.com>
2023-11-12 09:17:16 -0600

    added frontend pages

--- ride_share/user_auth/urls.py
+++ ride_share/user_auth/urls.py
@@ -5,4 +5,6 @@ from .views import SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", SignUpView.as_view(), name="user_login"),
+    path("signout/", SignUpView.as_view(), name="signout"),
 ]
